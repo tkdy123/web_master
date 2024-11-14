ae7e2d7968260419b9ed9f54cbd70ce08de2b733
unknown <pc>
2024-11-14 17:48:00 +0900

    save1

--- js/builtIn.js
+++ js/builtIn.js
@@ -2,6 +2,11 @@
 // 날짜(Date 객체)
 
 let today = new Date(); // Date객체.
+today.setFullYear(2023);
+today.setMonth(4);
+today.setDate(31);
+
+
 console.log(today);
 console.log(showTime(today));
 console.log(today.getFullYear()); // 년도.
@@ -17,8 +22,12 @@ function showTime(time = new Date()) {
   let HH = time.getHours();
   let mm = time.getMinutes();
   let ss = time.getSeconds(); // Number
-  console.log(('' + ss).length); // 2-> '222'
+  // console.log(('' + ss).length); // 2-> '222'
   ss = ('' + ss).length == 1 ? '0' + ss : ss; // 00 ~ 09
 
-  return yyyy + '-' + MM + '-' + dd + ' ' + HH + ':' + mm + ':' + ss;
+  // 월요일,
+  let day = time.getDay();
+  let days = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"]
+
+  return yyyy + '-' + MM + '-' + dd + ' ' + HH + ':' + mm + ':' + ss + '' + days[day];
 }
