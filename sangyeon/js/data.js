46ff912b01881f42a11f0f54eed439209e54df51
unknown <pc>
2024-11-22 17:48:17 +0900

    save

---
+++ sangyeon/js/data.js
@@ -0,0 +1,111 @@
+// id, pw, name
+const memberData = [{
+    id: 'user01',
+    pw: '1111',
+    name: '사용자1',
+    responsibility: 'User'
+  },
+  {
+    id: 'user02',
+    pw: '2222',
+    name: '사용자2',
+    responsibility: 'User'
+  },
+  {
+    id: 'user03',
+    pw: '3333',
+    name: '사용자3',
+    responsibility: 'User'
+  },
+  {
+    id: 'user04',
+    pw: '4444',
+    name: '사용자4',
+    responsibility: 'User'
+  },
+  {
+    id: 'user05',
+    pw: '5555',
+    name: '사용자5',
+    responsibility: 'Admin'
+  }
+]
+
+// pcode, pname, genre, image
+const movieData = [{
+    pcode: '001',
+    pname: '글레디에이터',
+    caption: '로마의 영웅이자 최고의 검투사였던 ‘막시무스’가 콜로세움...',
+    image: '글레디에이터.jpg'
+  },
+  {
+    pcode: '002',
+    pname: '사흘',
+    caption: '흉부외과의사 ‘승도’의 딸 ‘소미’가 구마의식 도중 목숨을 잃고...',
+    image: '사흘.jpg'
+  },
+  {
+    pcode: '003',
+    pname: '위키드',
+    caption: '신의 진정한 힘을 미처 발견하지 못한 엘파바(신시아 에리보) 자신의...',
+    image: '위키드.jpg'
+  },
+  {
+    pcode: '004',
+    pname: '청설',
+    caption: '손으로 설렘을 말하고 가슴으로 사랑을 느끼는, 청량한 설렘의...',
+    image: '청설.jpg'
+  },
+  {
+    pcode: '005',
+    pname: '히든페이스',
+    caption: '갇혔다 지켜봤다 벗겨졌다 지휘자 성진(송승헌)이 이끄는...',
+    image: '히든페이스.jpg'
+  },
+  {
+    pcode: '006',
+    pname: '4월이되면그녀는',
+    caption: '“아프고 슬프고 괴롭지만 그래도 사람들은 사랑에 빠진다” 결혼을...',
+    image: '4월이되면그녀는.jpg'
+  },
+  {
+    pcode: '007',
+    pname: '베놈',
+    caption: '죽음이 우리를 갈라놓을 때까지! 환상의 케미스트리의...',
+    image: '베놈.jpg'
+  },
+  {
+    pcode: '008',
+    pname: '블링크트와이스',
+    caption: '천국 같은 파티, 지옥 같은 진실! IT업계의 거물, 억만장자...',
+    image: '블링크트와이스.jpg'
+  }
+]
+
+// id, pcode, review
+const reviewData = [{
+    id: 'user01',
+    pcode: '001',
+    review: '재밌음'
+  },
+  {
+    id: 'user01',
+    pcode: '002',
+    review: '재밌음'
+  },
+  {
+    id: 'user02',
+    pcode: '002',
+    review: '재밌음'
+  },
+  {
+    id: 'user03',
+    pcode: '003',
+    review: '재밌음'
+  },
+  {
+    id: 'user04',
+    pcode: '003',
+    review: '재밌음'
+  }
+]
